c9b000303f202dc0dd0ad02a7cdf1643c02d07bd
rezichachkhiani <rezichachkhiani@gmail.com>
2025-04-20 22:52:27 +0400

    v.success

--- openGL1.py
+++ openGL1.py
@@ -1,149 +1,233 @@
 ﻿import pygame as pg
 from OpenGL.GL import *
-from OpenGL.GLU import gluOrtho2D
+from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
-import ctypes 
-from OpenGL.GL.shaders import compileProgram,compileShader
+import pyrr
 
-class app:
-    def __init__(self):
-        #initialize python
-        pg.init()
-        pg.font.init()
-        pg.display.set_mode((640,480),pg.OPENGL|pg.DOUBLEBUF)
-        self.clock=pg.time.Clock()
-        
-        #colors
-        glClearColor(0.1, 0.1, 0.1, 1.0)
-        
-        # Enable depth testing
-        glEnable(GL_DEPTH_TEST)
-        
-        self.shader = self.createShader("shaders/vertex.txt", "shaders/fragment.txt")
-        glUseProgram(self.shader)
-        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
 
-        self.triangle = Triangle()
-        self.wall_texture = Material("textures/wall.png")    
-        
-        self.offset_x = 0.0
-        self.offset_y = 0.0
+def create_shader(vertex_filepath: str, fragment_filepath: str) -> int:
+    with open(vertex_filepath, "r") as f:
+        vertex_src = f.readlines()
 
-        self.mainLoop()
-     
-    def createShader(self, vertexFilepath, fragmentFilepath):
-        with open(vertexFilepath, 'r') as f:
-            vertex_src = f.read()
+    with open(fragment_filepath, "r") as f:
+        fragment_src = f.readlines()
 
-        with open(fragmentFilepath, 'r') as f:
-            fragment_src = f.read()
+    shader = compileProgram(
+        compileShader(vertex_src, GL_VERTEX_SHADER),
+        compileShader(fragment_src, GL_FRAGMENT_SHADER),
+    )
+    return shader
 
-        shader = compileProgram(
-            compileShader(vertex_src, GL_VERTEX_SHADER),
-            compileShader(fragment_src, GL_FRAGMENT_SHADER)
+
+class Entity:
+    def __init__(self, position: list[float], eulers: list[float]):
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
+
+    def update(self) -> None:
+        self.eulers[1] += 0.25
+        if self.eulers[1] > 360:
+            self.eulers[1] -= 360
+
+    def get_model_transform(self) -> np.ndarray:
+        model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
+        model_transform = pyrr.matrix44.multiply(
+            m1=model_transform,
+            m2=pyrr.matrix44.create_from_axis_rotation(
+                axis=[0, 1, 0],
+                theta=np.radians(self.eulers[1]),
+                dtype=np.float32,
+            ),
+        )
+        return pyrr.matrix44.multiply(
+            m1=model_transform,
+            m2=pyrr.matrix44.create_from_translation(
+                vec=np.array(self.position), dtype=np.float32
+            ),
         )
 
-        return shader   
-    
-    def mainLoop(self):
+
+class App:
+    def __init__(self):
+        self._set_up_pygame()
+        self._set_up_timer()
+        self._set_up_opengl()
+        self._create_assets()
+        self._set_onetime_uniforms()
+        self._get_uniform_locations()
+
+    def _set_up_pygame(self) -> None:
+        pg.init()
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(
+            pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE
+        )
+        pg.display.set_mode((640, 480), pg.OPENGL | pg.DOUBLEBUF)
+
+    def _set_up_timer(self) -> None:
+        self.clock = pg.time.Clock()
+
+    def _set_up_opengl(self) -> None:
+        glClearColor(0.1, 0.2, 0.2, 1)
+        glEnable(GL_DEPTH_TEST)
+
+    def _create_assets(self) -> None:
+        self.cube = Entity(position=[0, 0, -3], eulers=[0, 0, 0])
+        self.cube_mesh = CubeMesh()
+        self.wood_texture = Material("gfx/wall.png")
+        self.shader = create_shader(
+            vertex_filepath="shaders/vertex.txt", fragment_filepath="shaders/fragment.txt"
+        )
+
+    def _set_onetime_uniforms(self) -> None:
+        glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy=45, aspect=640 / 480, near=0.1, far=10, dtype=np.float32
+        )
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader, "projection"),
+            1,
+            GL_FALSE,
+            projection_transform,
+        )
+
+    def _get_uniform_locations(self) -> None:
+        glUseProgram(self.shader)
+        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
+
+    def run(self) -> None:
         running = True
-        while(running):
-            #events
+        while running:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-             #refresh
-                    
-            keys = pg.key.get_pressed()
-            if keys[pg.K_LEFT]:
-                self.offset_x -= 0.01
-            if keys[pg.K_RIGHT]:
-                self.offset_x += 0.01
-            if keys[pg.K_UP]:
-                self.offset_y += 0.01
-            if keys[pg.K_DOWN]:
-                self.offset_y -= 0.01
+
+            self.cube.update()
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
             glUseProgram(self.shader)
-            self.wall_texture.use()
 
-            offset_location = glGetUniformLocation(self.shader, "offset")
-            glUniform3f(offset_location, self.offset_x, self.offset_y, 0.0)
-
-            glBindVertexArray(self.triangle.vao)
-            glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
+            glUniformMatrix4fv(
+                self.modelMatrixLocation,
+                1,
+                GL_FALSE,
+                self.cube.get_model_transform(),
+            )
+            self.wood_texture.use()
+            self.cube_mesh.arm_for_drawing()
+            self.cube_mesh.draw()
 
             pg.display.flip()
-            
-            #timing
             self.clock.tick(60)
-        self.quit()
-            
-    def quit(self):
-        self.triangle.destroy()
-        self.wall_texture.destroy()
+
+    def quit(self) -> None:
+        self.cube_mesh.destroy()
+        self.wood_texture.destroy()
         glDeleteProgram(self.shader)
         pg.quit()
 
-class Triangle:
+
+class CubeMesh:
     def __init__(self):
-        # Format: position (3 floats), color (3 floats), texture coords (2 floats)
-        self.vertices = np.array([
-            # Position         # Color          # Texture coordinates
-            [-0.5, -0.5, 0.0,  1.0, 1.0, 1.0,   0.0, 0.0],  # Bottom left
-            [ 0.5, -0.5, 0.0,  1.0, 1.0, 1.0,   1.0, 0.0],  # Bottom right
-            [ 0.0,  0.5, 0.0,  1.0, 1.0, 1.0,   0.5, 1.0]   # Top
-        ], dtype=np.float32)
-        
-        self.vertex_count = 3
+        vertices = (
+            -0.5, -0.5, -0.5, 0, 0,
+            0.5, -0.5, -0.5, 1, 0,
+            0.5,  0.5, -0.5, 1, 1,
+            0.5,  0.5, -0.5, 1, 1,
+           -0.5,  0.5, -0.5, 0, 1,
+           -0.5, -0.5, -0.5, 0, 0,
+           -0.5, -0.5,  0.5, 0, 0,
+            0.5, -0.5,  0.5, 1, 0,
+            0.5,  0.5,  0.5, 1, 1,
+            0.5,  0.5,  0.5, 1, 1,
+           -0.5,  0.5,  0.5, 0, 1,
+           -0.5, -0.5,  0.5, 0, 0,
+           -0.5,  0.5,  0.5, 1, 0,
+           -0.5,  0.5, -0.5, 1, 1,
+           -0.5, -0.5, -0.5, 0, 1,
+           -0.5, -0.5, -0.5, 0, 1,
+           -0.5, -0.5,  0.5, 0, 0,
+           -0.5,  0.5,  0.5, 1, 0,
+            0.5,  0.5,  0.5, 1, 0,
+            0.5,  0.5, -0.5, 1, 1,
+            0.5, -0.5, -0.5, 0, 1,
+            0.5, -0.5, -0.5, 0, 1,
+            0.5, -0.5,  0.5, 0, 0,
+            0.5,  0.5,  0.5, 1, 0,
+           -0.5, -0.5, -0.5, 0, 1,
+            0.5, -0.5, -0.5, 1, 1,
+            0.5, -0.5,  0.5, 1, 0,
+            0.5, -0.5,  0.5, 1, 0,
+           -0.5, -0.5,  0.5, 0, 0,
+           -0.5, -0.5, -0.5, 0, 1,
+           -0.5,  0.5, -0.5, 0, 1,
+            0.5,  0.5, -0.5, 1, 1,
+            0.5,  0.5,  0.5, 1, 0,
+            0.5,  0.5,  0.5, 1, 0,
+           -0.5,  0.5,  0.5, 0, 0,
+           -0.5,  0.5, -0.5, 0, 1,
+        )
+        self.vertex_count = len(vertices) // 5
+        vertices = np.array(vertices, dtype=np.float32)
 
         self.vao = glGenVertexArrays(1)
         glBindVertexArray(self.vao)
-
         self.vbo = glGenBuffers(1)
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-        
-        # Position attribute
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-        
-        # Color attribute
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
-        
-        # Texture coordinate attribute
-        glEnableVertexAttribArray(2)
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))  
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
 
-    def destroy(self):
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
+
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
+
+    def arm_for_drawing(self) -> None:
+        glBindVertexArray(self.vao)
+
+    def draw(self) -> None:
+        glDrawArrays(GL_TRIANGLES, 0, self.vertex_count)
+
+    def destroy(self) -> None:
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1, (self.vbo,))
- 
+
+
 class Material:
-    def __init__(self, filepath):
+    def __init__(self, filepath: str):
         self.texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.texture)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-
         image = pg.image.load(filepath).convert()
         image_width, image_height = image.get_rect().size
-        image_data = pg.image.tostring(image, "RGBA")
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+        img_data = pg.image.tostring(image, "RGBA")
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RGBA,
+            image_width,
+            image_height,
+            0,
+            GL_RGBA,
+            GL_UNSIGNED_BYTE,
+            img_data,
+        )
         glGenerateMipmap(GL_TEXTURE_2D)
 
-    def use(self):
+    def use(self) -> None:
         glActiveTexture(GL_TEXTURE0)
         glBindTexture(GL_TEXTURE_2D, self.texture)
 
-    def destroy(self):
+    def destroy(self) -> None:
         glDeleteTextures(1, (self.texture,))
 
-if __name__ == "__main__":
-    app()
-            
+
+my_app = App()
+my_app.run()
+my_app.quit()
+
